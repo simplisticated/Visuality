99577efd3e4533232027dbb271c93462eaf96c4d
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-10-30 11:52:25 +0300

    Updated demo project

--- Demo/Visuality/VisualityDemo/VisualityDemo/AppDelegate.swift
+++ Demo/Visuality/VisualityDemo/VisualityDemo/AppDelegate.swift
@@ -40,7 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window.backgroundColor = .white
             self.window = window
         }.move(toNavigationControllerOfType: UINavigationController.self, inWindow: window!, withConfigurationBlock: { (navigationController) in
-            let mainViewController = MainViewController.vt_viewControllerFromNibWithClassNameLocatedInMainBundle()
+            let mainViewController = MainViewController.from(.nibWithClassNameLocatedInMainBundle)
             
             navigationController.viewControllers = [
                 mainViewController
